19f461e94278a94b23e7d4f4337beae3f92bec87
joonk2 <with.joonk@gmail.com>
2026-02-16 00:33:23 +0900

    #ADD -> BE

---
+++ backend/src/main/java/com/kimchigang/ddayshare/DdayshareApplication.java
@@ -0,0 +1,13 @@
+package com.kimchigang.ddayshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdayshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdayshareApplication.class, args);
+	}
+
+}
